920a3aedbbe0b17731cde428f69a3023b2533279
chensl <chenshuanglong@ysten.com>
2018-08-15 16:01:14 +0800

    更新constraintAnimator的使用，新增AsyncListUtilDemo-未写具体内容

--- ConstraintLayout/src/main/java/com/jax/demos/constraintlayout/ConstraintAnimator.java
+++ ConstraintLayout/src/main/java/com/jax/demos/constraintlayout/ConstraintAnimator.java
@@ -33,8 +33,8 @@ public class ConstraintAnimator extends BaseActivity {
 
     public void onApplyClick(View view) {
 //        marginConnect();
-//        chainPacked();
-        testConnect();
+        chainPacked();
+//        testConnect();
         applyConstraintSet.applyTo(constraintLayout);
     }
 
